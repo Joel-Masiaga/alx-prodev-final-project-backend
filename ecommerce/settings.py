469d6439ab7bc3e52df0484e70d69e7ca5e11c97
Joel Masiaga <masiagajoel001@gmail.com>
2025-08-09 22:17:27 +0300

    Configuring whitenoise

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -128,9 +129,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -196,3 +203,4 @@ SIMPLE_JWT = {
 FLUTTERWAVE_SECRET_KEY = ''
 
 REACT_BASE_URL = os.getenv("REACT_BASE_URL", "http:/localhost:5173")
+
